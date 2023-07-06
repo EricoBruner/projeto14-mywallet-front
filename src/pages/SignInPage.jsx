6d306c923780864bfbca9e19e26cc295626bd486
EricoBruner <ericodasilvabruner@gmail.com>
2023-07-06 09:36:24 -0300

    feito ajuste para criação de cadastro

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,22 +1,26 @@
-import styled from "styled-components"
-import { Link } from "react-router-dom"
-import MyWalletLogo from "../components/MyWalletLogo"
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import MyWalletLogo from "../components/MyWalletLogo";
 
 export default function SignInPage() {
+  function signIn() {}
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={signIn}>
         <MyWalletLogo />
         <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <button>Entrar</button>
+        <input
+          placeholder="Senha"
+          type="password"
+          autocomplete="new-password"
+        />
+        <button type="submit">Entrar</button>
       </form>
 
-      <Link>
-        Primeira vez? Cadastre-se!
-      </Link>
+      <Link to={"/cadastro"}>Primeira vez? Cadastre-se!</Link>
     </SingInContainer>
-  )
+  );
 }
 
 const SingInContainer = styled.section`
@@ -25,4 +29,4 @@ const SingInContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,24 +1,71 @@
-import { Link } from "react-router-dom"
-import styled from "styled-components"
-import MyWalletLogo from "../components/MyWalletLogo"
+import { Link, redirect, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import MyWalletLogo from "../components/MyWalletLogo";
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignUpPage() {
+  const serverUrl = import.meta.env.VITE_API_SERVER;
+  const navigate = useNavigate();
+
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
+
+  function signUp(e) {
+    e.preventDefault();
+
+    if (password != confirmPass) return alert("Senhas não conferem!");
+
+    const user = { password, name, email };
+
+    axios
+      .post(`${serverUrl}/cadastro`, user)
+      .then((resp) => {
+        navigate("/home");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={signUp}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input
+          placeholder="Nome"
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <input
+          placeholder="E-mail"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          placeholder="Senha"
+          type="password"
+          autocomplete="new-password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          placeholder="Confirme a senha"
+          type="password"
+          autocomplete="new-password"
+          onChange={(e) => setConfirmPass(e.target.value)}
+          required
+        />
+        <button type="submit">Cadastrar</button>
       </form>
 
-      <Link>
-        Já tem uma conta? Entre agora!
-      </Link>
+      <Link>Já tem uma conta? Entre agora!</Link>
     </SingUpContainer>
-  )
+  );
 }
 
 const SingUpContainer = styled.section`
@@ -27,4 +74,4 @@ const SingUpContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
